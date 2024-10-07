0b39e1f4942917e1701934041817e7a690ad71ff
MegaJDraw <stars3r@gmail.com>
2024-10-07 05:26:54 -0500

    title

--- src/App.js
+++ src/App.js
@@ -60,6 +60,7 @@ function App() {
 
   return (
     <div className="container" ref={containerRef}>
+      <h1 className="title">Insight Engine</h1>
       <div className="blob blob-1"></div>
       <div className="blob blob-2"></div>
       <div className="blob blob-3"></div>
